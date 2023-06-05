b00b4b073b51f0f9e20f060a58334dd2dc192561
kedar242 <kutaskarkedar@gmail.com>
2023-06-05 12:34:56 +0530

    Added prompts for Java
    Added some prompts to perform basic operations on Array and String in java.

---
+++ java/StringDemo.java
@@ -0,0 +1,19 @@
+package java;
+
+public class StringDemo {
+    
+    // create a function to reverse a string
+    
+    // create a function to check whether a string is palindrome or not
+     
+    // create a function to convert lowercase to uppercase and vice versa
+
+    
+    // create a main method
+    public static void main(String[] args) {   
+        // create a string
+    
+    }
+
+
+}

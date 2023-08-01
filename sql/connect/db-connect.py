2ceb3636730b64dab8605e19e346c68e2147aa15
Sunil Sunil <govisun@amazon.com>
2023-08-01 21:25:53 +0000

    Squashed commit of the following
    commit b9f658c1 
    Author: Sunil Govindankutty <sunilkumarg@yahoo.com> 
    Date: June 15, 2023, 11:49 (UTC-04:00) 
    
        Database connectivity examples
    
    
    commit 4a0a9258 
    Author: Sunil Govindankutty <sunilkumarg@yahoo.com> 
    Date: June 13, 2023, 09:30 (UTC-04:00) 
    
        SQL workshop

---
+++ sql/connect/db-connect.py
@@ -0,0 +1,16 @@
+# Sample table schema:
+#CREATE TABLE Contacts ( 
+#  ID int, 
+#  LastName varchar(255), 
+#  FirstName varchar(255), 
+#  Address varchar(255), 
+#  City varchar(255), 
+#  State varchar(255), 
+#  ZipCode varchar(5), 
+#); 
+#
+
+
+
+
+

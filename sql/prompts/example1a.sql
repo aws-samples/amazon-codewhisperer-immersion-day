63c9e34ba0503ab0a79870bcd84b0babd29a7b99
Brian Beach <social@brianbeach.com>
2023-08-17 06:37:15 -0400

    updated initial sql files with latest prompts

--- sql/prompts/example1a.sql
+++ sql/prompts/example1a.sql
@@ -1,18 +1,8 @@
 /* Create a new database, StudentDemoDB, that contains three tables, 
-the Student_Info table that contains the students information,
-the Colleges table that contains a list of colleges, 
+the Student_Info table that contains the students first name, last name, age and major, 
+Colleges table that contains a list of colleges, 
 and finally the Student_Graduation table that contains the graduation year of each student.
 
 The Student_Graduation table contains two foreign keys; the ID of the student that references 
 the Student_Info table and the ID of the college that references the Colleges table.
 */
-
-
-
-
-
-
-
-
-
-

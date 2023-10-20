1f3d0a7c58770fe76a0a1967f1fa32c518a46bb2
Brian Beach <social@brianbeach.com>
2023-10-20 10:05:13 -0400

    Update java module

---
+++ java/src/main/java/com/amazon/aws/basics/CSVParser.java
@@ -0,0 +1,6 @@
+package com.amazon.aws.basics;
+
+public class CSVParser {   
+    //Parse a csv string and print the results
+    //columns include id,name and status    
+}
--- java/src/main/java/com/amazon/aws/basics/SongParser.java
+++
@@ -1,4 +0,0 @@
-package com.amazon.aws.basics;
-
-// Parse a CSV string of songs and return the list 
-//(position, originalChartDate, artists, title).
